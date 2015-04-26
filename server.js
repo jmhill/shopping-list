41b534f4225c87b2920910a58e670ca080ebc602
Justin Hill <justinmhill@gmail.com>
2015-04-26 11:00:32 -0400

    add delete and put endpoints

--- server.js
+++ server.js
@@ -34,6 +34,70 @@ app.post('/items', jsonParser, function(request, response) {
     response.status(201).json(item);
 });
 
+app.delete('/items', function(request, response){
+    var urlArray = request.url.split('/');
+    var itemID = parseInt(urlArray[2]);
+    // Delete items in list.
+    if (typeof itemID === 'number') {
+        // Grab the item id from the request url
+        var deletedItem;
+        // Go through the items array and splice out the deleted item.
+        items.items.forEach(function(object, index, itemArray){
+            if (object.id === itemID) {
+                deletedItem = object;
+                return itemArray.splice(index, 1);
+            } 
+        });
+        response.statusCode = 202; // Accepted
+        response.end(JSON.stringify(deletedItem));
+    } else {
+        response.statusCode = 404; //URI NOT FOUND
+        responseData = {'message': 'No item with that id was found'};
+        response.end(JSON.stringify(responseData));
+    }
+});
+
+app.put('/items', function(request, response){
+    // Edit items in list:
+    // User hits return and sends JSON data containing the new item object.
+    var item = '';
+    request.on('data', function (chunk) {
+        item += chunk;
+    });
+    request.on('end', function () {
+        try {
+            item = JSON.parse(item);
+        }
+        catch(e) {
+            response.statusCode = 400;
+            responseData = {'message': 'Invalid JSON'};
+            response.end(JSON.stringify(responseData));
+        }
+        // Check to make sure the item already exists by filtering the array by ID
+        var itemCheck = items.items.filter(function(element){
+            return element.id === item.id;
+        });
+        if (itemCheck.length > 0) {
+            // Once the new shopping item JSON object is received and parsed,
+            // go through the items array and replace the old item with the new.
+            items.items.forEach(function(object, index, itemArray) {
+                if (object.id == item.id) {
+                    itemArray[index] = item;
+                }
+            });
+            response.statusCode = 201; // Created
+            response.end(JSON.stringify(item)); // Send back the item
+        } else {
+            // This meets the requirements in the project, but 
+            // probably not a best practice to have the server just create the thing
+            // if there is an error.
+            items.add(item.name);
+            response.statusCode = 201; // Created
+            response.end();
+        }
+    });
+});
+
 app.listen(process.env.PORT || 8080);
 
 /*
